cac410b0a7c4517368d684b14d9acb3353d4dcb9
Teodor Moroz <teomoroz@gmail.com>
2019-03-01 07:40:42 +0200

    Added ColorExplorer to the package

---
+++ kagglePet/__init__.py
@@ -0,0 +1,1 @@
+from .color_exploration import ColorExplorer
--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='kagglepet',
     version='1.0',
     author='Teo',
+    url='https://github.com/thereisnosun/kagglePet'
     packages=['kagglePet']
 )
 
